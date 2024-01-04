a2fd2f592ffb1fc026f710934d56d26fb340d114
Javier Garcia <jgciudad@outlook.com>
2024-01-04 17:39:22 +0100

    corrected little bug in custom callback

--- SPINDLE/metrics.py
+++ SPINDLE/metrics.py
@@ -466,59 +466,58 @@ class BinaryWeightedCrossEntropy(tf.keras.losses.Loss):
         return ce
 
 class MyCustomCallback(tf.keras.callbacks.Callback):
-  
-  def __init__(self, validation_dataset, save_checkpoint_path, evaluation_rate, improvement_threshold, early_stopping_thr):
-      self.validation_dataset = validation_dataset
-      self.path = save_checkpoint_path
-      self.evaluation_rate = evaluation_rate
-      self.best_bal_acc = 0
-      self.improvement_threshold = improvement_threshold
-      self.counter = 0
-      self.early_stopping_thr = early_stopping_thr
-      self.history = []
+    def __init__(self, validation_dataset, save_checkpoint_path, evaluation_rate, improvement_threshold, early_stopping_thr):
+        self.validation_dataset = validation_dataset
+        self.path = save_checkpoint_path
+        self.evaluation_rate = evaluation_rate
+        self.best_bal_acc = 0
+        self.improvement_threshold = improvement_threshold
+        self.counter = 0
+        self.early_stopping_thr = early_stopping_thr
+        self.history = []
 
-      if os.path.isfile(os.path.join(self.path, "validation_log.txt")):
-        os.remove(os.path.join(self.path, "validation_log.txt"))
+        if os.path.isfile(os.path.join(self.path, "validation_log.txt")):
+            os.remove(os.path.join(self.path, "validation_log.txt"))
 
-  def on_train_batch_end(self, batch, logs=None):
+    def on_train_batch_end(self, batch, logs=None):
         
-        if self.counter < self.early_stopping_thr:
-            if batch % self.evaluation_rate == 0 and batch > 0:
-                print('Model validation')
-                y_true = []
-                y_pred = []
-                for i in range(len(self.validation_dataset)):
-                # for i in range(15): # shorter just for debugging
-                    x, y_true_batch = self.validation_dataset.__getitem__(i)
-                    y_true_batch = tf.convert_to_tensor(y_true_batch)
-                    y_pred_batch = self.model.predict(x, verbose=0)
-                    y_pred_batch = tf.one_hot(tf.math.argmax(y_pred_batch, axis=1), depth=3)
+        if batch % self.evaluation_rate == 0 and batch > 0:
+            print('Model validation')
+            y_true = []
+            y_pred = []
+            for i in range(len(self.validation_dataset)):
+            # for i in range(15): # shorter just for debugging
+                x, y_true_batch = self.validation_dataset.__getitem__(i)
+                y_true_batch = tf.convert_to_tensor(y_true_batch)
+                y_pred_batch = self.model.predict(x, verbose=0)
+                y_pred_batch = tf.one_hot(tf.math.argmax(y_pred_batch, axis=1), depth=3)
 
-                    y_true.append(y_true_batch)
-                    y_pred.append(y_pred_batch)
+                y_true.append(y_true_batch)
+                y_pred.append(y_pred_batch)
 
-                y_true = tf.concat(y_true, axis=0)
-                y_pred = tf.concat(y_pred, axis=0)
+            y_true = tf.concat(y_true, axis=0)
+            y_pred = tf.concat(y_pred, axis=0)
 
-                y_true = tf.math.argmax(y_true, axis=1)
-                y_pred = tf.math.argmax(y_pred, axis=1)
-                bal_acc = sklearn.metrics.balanced_accuracy_score(y_true=y_true, y_pred=y_pred)
+            y_true = tf.math.argmax(y_true, axis=1)
+            y_pred = tf.math.argmax(y_pred, axis=1)
+            bal_acc = sklearn.metrics.balanced_accuracy_score(y_true=y_true, y_pred=y_pred)
 
-                print('Validation balanced accuracy: ', bal_acc)
+            print('Validation balanced accuracy: ', bal_acc)
 
-                with open(os.path.join(self.path, "validation_log.txt"), "a") as text_file:
-                    text_file.write("{:g} {:g} \n".format(batch, bal_acc))
-                # self.history.append(bal_acc)
+            with open(os.path.join(self.path, "validation_log.txt"), "a") as text_file:
+                text_file.write("{:g} {:g} \n".format(batch, bal_acc))
+            # self.history.append(bal_acc)
 
-                if bal_acc - self.best_bal_acc > self.improvement_threshold:
-                    self.best_bal_acc = bal_acc
-                    self.counter = 0
-                    print('BEST MODEL UPDATED')
+            if bal_acc - self.best_bal_acc > self.improvement_threshold:
+                self.best_bal_acc = bal_acc
+                self.counter = 0
+                print('BEST MODEL UPDATED')
 
-                    self.model.save_weights(os.path.join(self.path, "best_model.h5"))
-                else:
-                    self.counter += 1
-        else:
+                self.model.save_weights(os.path.join(self.path, "best_model.h5"))
+            else:
+                self.counter += 1
+        
+        if self.counter >= self.early_stopping_thr:
             print('EARLY STOPPING ENABLED')
             # self.model.history.history['val_bal_acc'] = self.history
             self.model.stop_training = True
